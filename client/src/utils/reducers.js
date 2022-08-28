1571696d97e250805b95e326357b1f554cf84363
christopherstrolia-davis <chris.strolia-davis@swarmsolutions.com>
2022-08-28 19:08:19 -0400

    #2 - csd - Added shopping cart functionality
    - Add ability to add items to cart
    - Add ability to see cart from any page
    - Add ability to update or remove items from cart
    
    Resolves #2

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,18 +3,53 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from "./actions";
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
+
+const actions = {
+  default: (state) => state,
+  [UPDATE_PRODUCTS]: (state, action) => ({ ...state, products: [ ...action.products ] }),
+  [UPDATE_CATEGORIES]: (state, action) => ({ ...state, categories: [ ...action.categories ] }),
+  [UPDATE_CURRENT_CATEGORY]: (state, action) => ({ ...state, currentCategory: action.currentCategory }),
+  [ADD_TO_CART]: (state, action) => ({
+    ...state,
+    cartOpen: true,
+    cart: [ ...state.cart, action.product ],
+  }),
+  [ADD_MULTIPLE_TO_CART]: (state, action) => ({ ...state, cart: [ ...state.cart, ...action.products ] }),
+  [REMOVE_FROM_CART]: (state, action) => {
+    let newState = state.cart.filter(product => product._id !== action._id);
+
+    return {
+      ...state,
+      cartOpen: newState.length > 0,
+      cart: newState,
+    };
+  },
+  [UPDATE_CART_QUANTITY]: (state, action) => ({
+    ...state,
+    cartOpen: true,
+    cart: state.cart.map(product => {
+      const purchaseQuantity = action._id === product._id
+        ? action.purchaseQuantity
+        : product.purchaseQuantity;
+
+      return { ...product, purchaseQuantity };
+    }),
+  }),
+  [CLEAR_CART]: (state) => ({ ...state, cartOpen: false, cart: [] }),
+  [TOGGLE_CART]: (state) => ({ ...state, cartOpen: !state.cartOpen }),
+};
 
 export const reducer = (state, action) => {
-  const defaultAction = () => state;
-  const actions = {
-    [UPDATE_PRODUCTS]: () => ({ ...state, products: [ ...action.products ] }),
-    [UPDATE_CATEGORIES]: () => ({ ...state, categories: [ ...action.categories ] }),
-    [UPDATE_CURRENT_CATEGORY]: () => ({ ...state, currentCategory: action.currentCategory }),
-  };
-  const exec = actions[action.type] ?? defaultAction;
+  const exec = actions[action.type] ?? actions.default;
 
-  return exec();
+  return exec(state, action);
 };
 
 export function useProductReducer(initialState) {
